267a4135169a00ff078e560e343e70a6b6e19849
vinah331 <vinah331@student.liu.se>
2021-03-26 09:48:58 +0100

    tokens added to GameLanguage

--- rdparse.rb
+++ rdparse.rb
@@ -238,79 +238,34 @@ class GameLanguage
 
   def initialize
     @gameParser = Parser.new("game language") do
-      token()
-
-  end
-
-class DiceRoller
-
-  def DiceRoller.roll(times, sides)
-    (1..times).inject(0) {|sum, _| sum + rand(sides) + 1 }
-  end
-
-  def initialize
-    @diceParser = Parser.new("dice roller") do
-      token(/\s+/)
-      token(/\d+/) {|m| m.to_i }
-      token(/./) {|m| m }
-
-
-      start :expr do
-        match(:expr, '+', :term) {|a, _, b| a + b }
-        match(:expr, '-', :term) {|a, _, b| a - b }
-        match(:term)
-      end
-
-      rule :term do
-        match(:term, '*', :dice) {|a, _, b| a * b }
-        match(:term, '/', :dice) {|a, _, b| a / b }
-        match(:dice)
-      end
-
-      rule :dice do
-        match(:atom, 'd', :sides) {|a, _, b| DiceRoller.roll(a, b) }
-        match('d', :sides) {|_, b| DiceRoller.roll(1, b) }
-        match(:atom)
-      end
-
-      rule :sides do
-        match('%') { 100 }
-        match(:atom)
-      end
-
-      rule :atom do
-        # Match the result of evaluating an integer expression, which
-        # should be an Integer
-        match(Integer) {|a| a}
-        match('(', :expr, ')') {|_, a, _| a }
-      end
+      token(/\s+/) #removes whitespaces
+      #token(/\d+/) {|m| m.to_i } #returns numbers as integers
+      token(/\w+/) {|m| m } #returns chars
+      token(/./) {|m| m } #returns rest
     end
   end
 
-  def done(str)
-    ["quit","exit","bye",""].include?(str.chomp)
-  end
-
   def roll
-    print "[diceroller] "
+    print "[gameParser] "
     str = gets
     if done(str) then
       puts "Bye."
     else
-      puts "=> #{@diceParser.parse str}"
+      puts "=> #{@gameParser.parse str}"
       roll
     end
   end
 
   def log(state = true)
     if state
-      @diceParser.logger.level = Logger::DEBUG
+      @gameParser.logger.level = Logger::DEBUG
     else
-      @diceParser.logger.level = Logger::WARN
+      @gameParser.logger.level = Logger::WARN
     end
   end
 end
 
+GameLanguage.new.roll
 
 class LogicExpr
   def initialize
@@ -357,24 +312,24 @@ class LogicExpr
     ["quit","exit","bye",""].include?(str.chomp)
   end
 
-  def roll
-    print "[logixexpr] "
-    str = gets
-    if done(str) then
-      puts "Bye."
-    else
-      puts "=> #{@LogicParser.parse str}"
-      roll
+    def roll
+      print "[logixexpr] "
+      str = gets
+      if done(str) then
+        puts "Bye."
+      else
+        puts "=> #{@LogicParser.parse str}"
+        roll
+      end
     end
-  end
 
-  def log(state = true)
-    if state
-      @diceParser.logger.level = Logger::DEBUG
-    else
-      @diceParser.logger.level = Logger::WARN
+    def log(state = true)
+      if state
+        @diceParser.logger.level = Logger::DEBUG
+      else
+        @diceParser.logger.level = Logger::WARN
+      end
     end
-  end
 end
 # Examples of use
 
